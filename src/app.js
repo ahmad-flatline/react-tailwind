274534e1c9aceee4a6c2f1d87458a39e031cf64f
Ahmad - Flatline <ahmad@flatlineagency.com>
2022-10-08 14:34:30 +0200

    Added a transition container and transition components.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 // import logo from './logo.svg'; // <img src={logo} className="App-logo" alt="logo" />
+import { useEffect, useState } from "react";
+// import TransitionContainer from "./component/transition-container";
+import TodoList from "./page/todo-list";
 import "./app.css";
 
 function App() {
+  const [show, serShow] = useState(true);
+
+  useEffect(() => {
+    serShow(false);
+  }, []);
+
   return (
     <div className="">
       <header className="">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        {show && <div>Hello</div>}
       </header>
+      <TodoList />
     </div>
   );
 }
